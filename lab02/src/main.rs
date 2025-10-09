a496127642f8119920930e70e5dfc5affc716e94
razx11111 <rgheoca@yahoo.com>
2025-10-09 12:42:07 +0300

    god I hate floats

--- lab02/src/main.rs
+++ lab02/src/main.rs
@@ -24,7 +24,7 @@ fn int_len(mut n:u32) -> u8 {
     len
 }
 
-fn add_integer(s: &mut String, mut n: i32) {
+fn add_integer(s: &mut String, mut n: i32, is_float: bool) {
     if n < 0 {
         s.push('-');
         n *= -1;
@@ -35,7 +35,7 @@ fn add_integer(s: &mut String, mut n: i32) {
     let mut count:u8 = 0;
     
     while exp != 0 {
-        if count % 3 == 0 && count != 0 {
+        if count % 3 == 0 && count != 0 && is_float == false {
             s.push('_');
         }
         s.push((((n / exp) % 10) as u8 + '0' as u8) as char); //idk daca trb atatea paranteze but why not
@@ -44,22 +44,23 @@ fn add_integer(s: &mut String, mut n: i32) {
     }
 }
 
-fn pnct_index(mut n: f32) -> u8 {
-    let mut index:u8 = 0;
-    while n > 0.1 {
-        n /= 10.0;
-        index += 1;
-    }
-    index - 1
-}
-
-fn add_float( s: &mut String, n: f32) {
-    if n < 0 {
+fn add_float( s: &mut String, mut n: f64) {
+    if n < 0.0 {
         s.push('-');
-        n *= -1;
+        n *= -1.0;
     }
-    
 
+    let int_part:i32 = n as i32;
+    add_integer(s, int_part, true);
+    s.push('.');
+    let mut forward:f64 = n*10.0;
+    
+    while forward % 10.0 != 0.0 {
+        let digit = (forward as i32) % 10;
+        s.push((digit as u8 + '0' as u8) as char);
+        forward *= 10.0;
+        n*=10.0;
+    }
 }
 
 fn main() {
@@ -71,6 +72,7 @@ fn main() {
 
         i += 1;
     }
-    add_integer(&mut s, -323443);
+    add_integer(&mut s, -323443, false);
+    add_float(&mut s, -7434.14785);
     print!("{}", s);
 }
